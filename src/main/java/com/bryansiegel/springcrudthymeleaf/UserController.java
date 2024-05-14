c7289b24537e78e556000a8eb973f9a9a2f3a425
Bryan Siegel <bryansiegel@gmail.com>
2024-05-14 16:07:45 -0700

    added controller actions

--- src/main/java/com/bryansiegel/springcrudthymeleaf/UserController.java
+++ src/main/java/com/bryansiegel/springcrudthymeleaf/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -34,6 +35,25 @@ public class UserController {
         }
 
         userRepository.save(user);
+        return "redirect:/index";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+
+        userRepository.save(user);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User with id " + id + " not found"));
+        userRepository.delete(user);
         return "redirect:/index";
     }
 }
